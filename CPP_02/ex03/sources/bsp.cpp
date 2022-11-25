5f437dbd6c133571254fa81ff4a609ee90bbfa5c
Diogo andre Candeias antunes <dcandeia@c1r18s6.42lisboa.com>
2022-11-25 10:31:46 +0000

    finished function bsp to check if a point is or not inside a triangle

--- CPP_02/ex03/sources/bsp.cpp
+++ CPP_02/ex03/sources/bsp.cpp
@@ -6,14 +6,20 @@ static Fixed	getTriangleArea(const Point p1, const Point p2, const Point p3);
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
 	Fixed	totalArea;
-	// Fixed	area1;
-	// Fixed	area2;
-	// Fixed	area3;
+	Fixed	area1;
+	Fixed	area2;
+	Fixed	area3;
 
 	(void)point;
 	totalArea = getTriangleArea(a, b, c);
-	std::cout << "Triangle area 1 = " << totalArea.toFloat() << std::endl;
-	return (1);
+	area1 = getTriangleArea(a, b, point);
+	area2 = getTriangleArea(a, c, point);
+	area3 = getTriangleArea(b, c, point);
+	if (area1.toFloat() == 0 || area2.toFloat() == 0 || area3.toFloat() == 0)
+		return (false);
+	if (totalArea.toFloat() != area1.toFloat() + area2.toFloat() + area3.toFloat())
+		return (false);
+	return (true);
 }
 
 static Fixed	getTriangleArea(const Point p1, const Point p2, const Point p3)
@@ -23,14 +29,11 @@ static Fixed	getTriangleArea(const Point p1, const Point p2, const Point p3)
 	Fixed	resultX2;
 	Fixed	resultX3;
 
-	std::cout << "P1 - " << p1.getXFixedPoint() << " / " << p1.getYFixedPoint() << std::endl;
-	std::cout << "P2 - " << p2.getXFixedPoint() << " / " << p2.getYFixedPoint() << std::endl;
-	std::cout << "P3 - " << p3.getXFixedPoint() << " / " << p3.getYFixedPoint() << std::endl;
 	resultX1 = p1.getXFixedPoint() * (p2.getYFixedPoint() - p3.getYFixedPoint());
 	resultX2 = p2.getXFixedPoint() * (p3.getYFixedPoint() - p1.getYFixedPoint());
 	resultX3 = p3.getXFixedPoint() * (p1.getYFixedPoint() - p2.getYFixedPoint());
 	areaFixedPoint =  (resultX1 + resultX2 + resultX3) / 2;
-	if (areaFixedPoint.toFloat() < 0)
+	if (areaFixedPoint < 0)
 		areaFixedPoint = Fixed(-1) * areaFixedPoint;
 	return (areaFixedPoint);
 }
--- CPP_02/ex03/sources/main.cpp
+++ CPP_02/ex03/sources/main.cpp
@@ -5,10 +5,45 @@ bool bsp(Point const a, Point const b, Point const c, Point const point);
 
 int main()
 {
-	Point	p1(1.3f, 1.0f);
-	Point	p2(1.3f, -2.0f);
-	Point	p3(6.4f, 0.0f);
-	Point	pFind(1.0f, 1.0f);
+	Point	p1(0.0f, 0.0f);
+	Point	p2(0.0f, -2.0f);
+	Point	p3(6.0f, 0.0f);
+	Point	pFind_0(0.0f, 0.0f);
+	Point	pFind_1(0.0f, 1.0f);
+	Point	pFind_2(3.5f, -0.25f);
+	Point	pFind_3(1.25f, -0.02f);
 
-	bsp(p1, p2, p3, pFind);
+	std::cout << std::endl;
+
+	std::cout << "Point 2 - " << pFind_0.getXFixedPoint() << " / " << pFind_0.getYFixedPoint() <<std::endl;
+	if (bsp(p1, p2, p3, pFind_0) == true)
+		std::cout << "The point is inside the triangle!" << std::endl;
+	else
+		std::cout << "The point is not inside the triangle!" << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "Point 2 - " << pFind_1.getXFixedPoint() << " / " << pFind_1.getYFixedPoint() <<std::endl;
+	if (bsp(p1, p2, p3, pFind_1) == true)
+		std::cout << "The point is inside the triangle!" << std::endl;
+	else
+		std::cout << "The point is not inside the triangle!" << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "Point 3 - " << pFind_2.getXFixedPoint() << " / " << pFind_2.getYFixedPoint() <<std::endl;
+	if (bsp(p1, p2, p3, pFind_2) == true)
+		std::cout << "The point is inside the triangle!" << std::endl;
+	else
+		std::cout << "The point is not inside the triangle!" << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "Point 4 - " << pFind_3.getXFixedPoint() << " / " << pFind_3.getYFixedPoint() <<std::endl;
+	if (bsp(p1, p2, p3, pFind_3) == true)
+		std::cout << "The point is inside the triangle!" << std::endl;
+	else
+		std::cout << "The point is not inside the triangle!" << std::endl;
+
+	std::cout << std::endl;
 }
