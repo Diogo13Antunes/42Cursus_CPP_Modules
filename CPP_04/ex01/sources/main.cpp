54011d69d40cb21c8293f205f6797694429959a9
Diogo andre Candeias antunes <dcandeia@c1r19s2.42lisboa.com>
2022-11-29 12:13:37 +0000

    removed main.hpp and changed main.cpp and Makefile

---
+++ CPP_04/ex01/sources/main.cpp
@@ -0,0 +1,50 @@
+#include <Animal.hpp>
+#include <Cat.hpp>
+#include <Dog.hpp>
+#include <WrongCat.hpp>
+
+int main()
+{
+	Animal *animal[4];
+
+	std::cout << "=======================================" << std::endl;
+
+	for (int i = 0; i < 2; i++)
+		animal[i] = new Dog();
+
+	std::cout << "=======================================" << std::endl;
+
+	for (int i = 2; i < 4; i++)
+		animal[i] = new Cat();
+
+	std::cout << "=======================================" << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "=======================================" << std::endl;
+
+	for (int i = 0; i < 4; i++)
+	{
+		animal[i]->getType();
+		animal[i]->makeSound();
+	}
+	
+	std::cout << "=======================================" << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "=======================================" << std::endl;
+	
+	for (int i = 0; i < 4; i++)
+	{
+		if (i == 2)
+			std::cout << "=======================================" << std::endl;
+		delete animal[i];
+	}
+
+	std::cout << "=======================================" << std::endl;
+
+	std::cout << std::endl;
+
+	return (0);
+}
