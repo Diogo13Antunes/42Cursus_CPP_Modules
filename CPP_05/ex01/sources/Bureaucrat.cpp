86e1ea7e0243927859474d5ac5032026ded1a86f
Diogo andre Candeias antunes <dcandeia@c1r18s4.42lisboa.com>
2022-12-05 16:54:47 +0000

    CPP_05/ex01 -> Added files from the previous exercise

---
+++ CPP_05/ex01/sources/Bureaucrat.cpp
@@ -0,0 +1,82 @@
+#include <Bureaucrat.hpp>
+
+Bureaucrat::Bureaucrat(): _name("Bureaucrat_Name"), _grade(1)
+{
+	std::cout << "Default Bureaucrat Constructor" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string name, const int grade):
+	_name(name)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
+	else
+		_grade = grade;
+	std::cout << "Grade Bureaucrat Constructor" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &src)
+{
+	*this = src;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Default Bureaucrat Destructor" << std::endl;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &src)
+{
+	this->_grade = src._grade;
+	return (*this);
+}
+
+const char *Bureaucrat::GradeTooHighException::what(void) const throw()
+{
+	return ("Grade is too Hight");
+}
+
+const char *Bureaucrat::GradeTooLowException::what(void) const throw()
+{
+	return ("Grade is too Low");
+}
+
+int		Bureaucrat::getGrade(void) const
+{
+	return (this->_grade);
+}
+
+const std::string	Bureaucrat::getName(void) const
+{
+	return (this->_name);
+}
+
+void	Bureaucrat::gradeIncrement(void)
+{
+	int temp = this->_grade - 1;
+
+	if (temp < 1)
+		throw GradeTooHighException();
+	else
+		this->_grade = temp;
+}
+
+void	Bureaucrat::gradeDecrement(void)
+{
+	int temp = this->_grade + 1;
+
+	if (temp > 150)
+		throw GradeTooLowException();
+	else
+		this->_grade = temp;
+}
+
+std::ostream    &operator<< (std::ostream &os, const Bureaucrat &src)
+{
+	os << src.getName();
+	os << ", bureaucrat grade ";
+	os << src.getGrade();
+	return (os);
+}
