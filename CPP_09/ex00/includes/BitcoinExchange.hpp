1d278bed77069f6916cff9721f74c35803cfa322
Diogo andre Candeias antunes <dcandeia@c2r5s3.42lisboa.com>
2023-03-17 11:10:16 +0000

    Added exercise initial required files

---
+++ CPP_09/ex00/includes/BitcoinExchange.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class BitcoinExchange
+{
+	public:
+		BitcoinExchange &operator=(const BitcoinExchange &src);
+		BitcoinExchange(const BitcoinExchange &src);
+		BitcoinExchange(void);
+		~BitcoinExchange(void);
+};
---
+++ CPP_09/ex00/sources/BitcoinExchange.cpp
@@ -0,0 +1,21 @@
+#include <BitcoinExchange.hpp>
+
+BitcoinExchange::BitcoinExchange(void)
+{
+	//Default BitcoinExchange Constructor
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &src)
+{
+	//BitcoinExchange Copy Constructor
+}
+
+BitcoinExchange::~BitcoinExchange(void)
+{
+	//Default BitcoinExchange Destructor
+}
+
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &src)
+{
+	//BitcoinExchange Copy Assignment Operator
+}
